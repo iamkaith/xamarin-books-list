83932e7e3e066868957e03fdb1af4064b74b3b83
iamkaith <klramirez665@gmail.com>
2020-02-27 01:42:59 -0800

    Add stock images into an assets folder

---
+++ BooksListTutorial/ImageResourceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+using System.Reflection;
+
+namespace BooksListTutorial
+{
+  [Preserve(AllMembers = true)]
+  [ContentProperty("Source")]
+  public class ImageResourceExtension : IMarkupExtension
+  {
+    public string Source { get; set; }
+    public object ProvideValue(IServiceProvider serviceProvider) {
+      if(Source == null) {
+        return null;
+      }
+
+      var imageSource = ImageSource.FromResource(Source, Assembly.GetExecutingAssembly());
+      return imageSource;
+    }
+  }
+}
