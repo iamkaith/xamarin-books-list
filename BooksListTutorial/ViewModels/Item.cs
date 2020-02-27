3e217cd81767c242ecc5767d2d3f21a63f0a054f
iamkaith <klramirez665@gmail.com>
2020-02-27 13:53:56 -0800

    Setup viewmodel and data

---
+++ BooksListTutorial/ViewModels/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksListTutorial.ViewModels
+{
+  public class Item
+  {
+    public string Text { get; set; }
+    public string Detail { get; set; }
+    public string URL { get; set; }
+    public override string ToString() => URL; 
+  }
+}
---
+++ BooksListTutorial/ViewModels/ItemGrouping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BooksListTutorial.ViewModels
+{
+  public class ItemGrouping<K, T>: ObservableCollection<T>
+  {
+    public K Key { get; private set; }
+
+    public ItemGrouping(K key, IEnumerable<T> items) {
+      Key = key;
+      foreach(var item in items) {
+        this.Items.Add(item);
+      }
+    }
+  }
+}
